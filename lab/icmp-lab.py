106e8ae47ee2cf8c7200f25170afb64c0348f534
TLRKiliann <philogenie@protonmail.com>
2026-05-05 15:48:22 +0200

    explanation design improved

--- lab/icmp-lab.py
+++ lab/icmp-lab.py
@@ -3,32 +3,26 @@
 from scapy.all import *
 import time
 
-text = '''
+text = """
+######################################################
 send() - Couche 3 (Réseau/IP)
 Travaille au niveau IP (pas besoin de couche Ethernet)
 Envoie des paquets (datagrammes IP)
 Le système détermine automatiquement l'Ethernet/MAC
 Nécessite seulement la couche IP et au-dessus
-
+------------------------------------------------------
 sendp() - Couche 2 (Liaison/Ethernet)
 Travaille au niveau Ethernet (frame complète)
-Envoie des trames (ye need MAC source/destination)
+Envoie des trames (adresse MAC source/destination)
 Vous devez spécifier l'interface
 Nécessite explicitement la couche Ethernet
-'''
+######################################################
+"""
 print(text)
 
-# Construction du paquet
-pkt = Ether()/IP()/TCP(dport=80);
-print(pkt.summary());
+fake_mac = "de:ad:be:ef:ca:fe"
 
-print(pkt.sprintf("%Ether.src% > %IP.src%"));
-
-print("---\n");
-
-fausse_mac = "de:ad:be:ef:ca:fe"
-
-pktS = Ether(src=fausse_mac)/IP(dst="8.8.8.8")/ICMP()
+pktS = Ether(src=fake_mac)/IP(dst="8.8.8.8")/ICMP()
 
 sniffer = AsyncSniffer(filter="icmp and host 8.8.8.8", iface="en1", timeout=5)
 sniffer.start()
@@ -48,32 +42,6 @@ else:
     print("❌ Pas de réponse")
 
 '''
-tcpdump CMD => https://hackertarget.com/tcpdump-examples/
-===========
-
-$ sudo tcpdump -i eth0 -nn -s0 -c 1 -v -X -G 5 port 80
-
--A = ASCII
--X = hexadecimal & ASCII
-
--c = nb de paquet
-
--G = temps d'attente en sec 
-
--nn = A single (n) will not resolve hostnames. A double (nn) will not resolve hostnames or ports. 
-This is handy for not only viewing the IP / port numbers but also when capturing a large amount 
-of data, as the name resolution will slow
-
--v : Verbose, using (-v) or (-vv) increases the amount of detail shown in the output, often showing 
-more protocol specific information.
-
-request:
-sudo tcpdump -i en1 -c 1 -v -X 'icmp[icmptype] != icmp-echoreply'
-
-reply:
-sudo tcpdump -i en1 -c 1 -v -X 'icmp[icmptype] != icmp-echo'
-
-
 sudo tcpdump 'tcp[tcpflags] & (tcp-syn|tcp-fin) != 0
 sudo tcpdump -i en0 -n host 8.8.8.8
 sudo tcpdump -i en0 -n icmp -w capture.pcap
