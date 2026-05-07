dd68b209f090931ba0bf827d01a679064c0a40ce
TLRKiliann <philogenie@protonmail.com>
2026-05-07 16:49:27 +0200

    under dev

--- lab/tcp_scan.py
+++ lab/tcp_scan.py
@@ -2,5 +2,16 @@
 
 from scapy.all import *
 
-pkt = sr1(IP(dst="192.168.18.1")/TCP(dport=80, flags="S"), timeout=1, inter=0.5)
+# Linux
+# sudo ./bin/python3 tcp_scan.py
 
+width = 60
+title = "Port Scan with TCP & sr1"
+print(width * "#")
+print()
+print(title.center(width))
+print()
+print(width * "#")
+
+pkt = sr1(IP(dst="192.168.18.1")/TCP(dport=80, flags="S"), timeout=3, inter=0.5, retry=0)
+pkt.show()
