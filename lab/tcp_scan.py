8b1dc55bc304121fa41edbdb8bf3fad864728357
TLRKiliann <philogenie@protonmail.com>
2026-05-08 14:53:42 +0200

    script tested & done

--- lab/tcp_scan.py
+++ lab/tcp_scan.py
@@ -1,17 +1,53 @@
 #!/usr/bin/env python3
 
+import sys
 from scapy.all import *
 
 # Linux
 # sudo ./bin/python3 tcp_scan.py
 
 width = 60
-title = "Port Scan with TCP & sr1"
+title = "Port Scan with TCP & sr"
 print(width * "#")
 print()
 print(title.center(width))
 print()
 print(width * "#")
 
-pkt = sr1(IP(dst="192.168.18.1")/TCP(dport=80, flags="S"), timeout=3, inter=0.5, retry=0)
-pkt.show()
+try:
+    ans, unans = sr(IP(dst="192.168.18.1")/TCP(dport=[22,80,443], flags="S"), timeout=3, inter=0.5, retry=0)
+
+    for sent, received in ans:
+        if received.haslayer(IP):
+            print(f"Response from {received[IP].src}")
+
+        if not received.haslayer(TCP):
+            print("  |- Not a TCP response")
+            continue
+
+        received.summary()
+        print(f" |- Src port: {received[TCP].sport}")
+        print(f" |- Dst port: {received[TCP].dport}")
+        print(f" |- Sequence number: {received[TCP].seq}")
+        print(f" |- Acknowledgment number: {received[TCP].ack}")
+        print(f" |- Data Offset:{received[TCP].dataofs}")
+        print(f" |- Reserved: {received[TCP].reserved}")
+
+        flag = received[TCP].flags
+        if flag == 0x12:
+            print(" |- Flags: SA (SYN-ACK) → Port OPEN")
+        elif flag == 0x14:
+            print(" |- Flags: RA (RST-ACK) → Port CLOSED")
+        else:
+            print(f" |- Flags: {flag}")
+
+        print(f" |- Window: {received[TCP].window}")
+        print(f" |- Checksum: {received[TCP].chksum}")
+        print(f" |- Urgent Pointer: {received[TCP].urgptr}")
+
+except Exception as e:
+    print(f"An error occured => {e}")
+    sys.exit(1)
+
+print(f"\n✅ Analysis completed: {len(ans)} responses, {len(unans)} unanswered")
+sys.exit(0)
