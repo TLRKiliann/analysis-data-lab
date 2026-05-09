69916792ecc9b8bb76e69d84a676e279853a78b7
TLRKiliann <philogenie@protonmail.com>
2026-05-09 13:29:13 +0200

    done

--- lab/icmp-lab.py
+++ lab/icmp-lab.py
@@ -1,45 +1,27 @@
 #!/usr/bin/env python3
 
+import sys
 from scapy.all import *
-# from scapy import Ether, IP, ICMP, AsyncSniffer, sendp
-import time
 
-text = """
-######################################################
-send() - Couche 3 (Réseau/IP)
-Travaille au niveau IP (pas besoin de couche Ethernet)
-Envoie des paquets (datagrammes IP)
-Le système détermine automatiquement l'Ethernet/MAC
-Nécessite seulement la couche IP et au-dessus
-------------------------------------------------------
-sendp() - Couche 2 (Liaison/Ethernet)
-Travaille au niveau Ethernet (frame complète)
-Envoie des trames (adresse MAC source/destination)
-Vous devez spécifier l'interface
-Nécessite explicitement la couche Ethernet
-######################################################
-"""
-print(text)
+# sudo ./bin/python3 icmp-lab.py
+
+width = 60
+text_icmp = "ICMP as PING"
+print(width * "#")
+print()
+print(text_icmp.center(width))
+print()
+print(width * "#")
 
 fake_mac = "de:ad:be:ef:ca:fe"
+fake_ip = "192.168.18.44"
 
-pktS = Ether(src=fake_mac)/IP(dst="8.8.8.8")/ICMP()
+# verify your internet interface
+iface = "eth0"
 
-sniffer = AsyncSniffer(filter="icmp and host 8.8.8.8", iface="en1", timeout=5)
-sniffer.start()
+response = sr1(IP(dst="8.8.8.8")/ICMP(), timeout=3, iface=iface, verbose=True)
 
-time.sleep(0.5)
-sendp(pktS, iface="en1", verbose=True)
-
-time.sleep(2)
-sniffer.stop()
-
-if len(sniffer.results) > 1:
-    print(f"✅ Réponse reçue ! {len(sniffer.results)} paquets capturés")
-    for pkt in sniffer.results:
-        if ICMP in pkt and pkt[ICMP].type == 0:
-            print(f"   Réponse de {pkt[IP].src}")
+if response:
+    print(f"✅ IP dst response ok !")
 else:
-    print("❌ Pas de réponse")
-
-print("end !")
+    print(f"❌ No response from IP dst...")
